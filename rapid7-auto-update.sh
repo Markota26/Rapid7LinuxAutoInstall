a996189ec2f345ec707acce09dd017a2a431f972
Lucas Esmeraldino <133903212+esmeraldino-lk@users.noreply.github.com>
2025-04-22 11:02:06 -0300

    Update rapid7-auto-update.sh

--- rapid7-auto-update.sh
+++ rapid7-auto-update.sh
@@ -6,8 +6,18 @@ HOSTNAME=$(hostname)
 DATE=$(date '+%d/%m/%Y %H:%M:%S')
 CRONTAB=$(crontab -l)
 CRON_HORARIO="0 7 * * *"
-CRON_CMD="$CRON_HORARIO bash $SCRIPT"
+CRON_CMD="$CRON_HORARIO bash /opt/rapid7i/rapid7rotine.sh"
+ROTINECOMMAND="bash /opt/rapid7i/rapid7i.sh"
 OSVERSION="$(cat /etc/os-release | grep NAME | sed 's/NAME="//' | sed 's/"//' | sed 's/PRETTY_//g' | sed 's/ GNU*.*//g' | head -1)"
+
+if [[ -z $3 ]]; then
+    echo -e "$errorCheck Usage: $0 <token> <region> <api-key>"
+    echo -e "$errorCheck Token: $1"
+    echo -e "$errorCheck Region: $2"
+    echo -e "$errorCheck Api-key: $3"
+    exit 1
+fi
+
 if [[ $OSVERSION == "" ]];then
 	OSVERSION="$(cat /etc/issue | head -1)"
 fi
@@ -18,9 +28,9 @@ cp $0 /opt/rapid7i/rapid7i.sh
 curl "$URL" > $SCRIPT
 
 bash $SCRIPT $1 $2 $3
-(crontab -l 2>/dev/null | grep -F "$SCRIPT $1 $2 $3") >/dev/null
+echo "$ROTINECOMMAND" $1 $2 $3 > /opt/rapid7i/rapid7rotine.sh
 
-if crontab -l 2>/dev/null | grep -q "$SCRIPT"; then
+if crontab -l 2>/dev/null | grep -q "rapid7rotine.sh"; then
     echo -e "{\"date\":\"$DATE\",\"hostname\":\"${HOSTNAME}\",\"status\":\"Cron update already set\"}" >> /opt/rapid7i/cron.log
     echo -e "[!] Cron update already set"
 else
